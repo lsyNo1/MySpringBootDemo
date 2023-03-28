b1763e042f74ad2fa0d6f9c2e845513672ba2752
lsy <91182641+lsyNo1@users.noreply.github.com>
2023-03-28 16:04:39 +0800

    add basic file

---
+++ src/test/java/com/lsy/MySpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
