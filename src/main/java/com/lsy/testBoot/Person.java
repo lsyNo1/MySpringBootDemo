600d9a081f3ab93f6e8d25e6cce9e2e1319aa6af
lsy <91182641+lsyNo1@users.noreply.github.com>
2023-04-09 14:40:31 +0800

    add config file

---
+++ src/main/java/com/lsy/testBoot/Person.java
@@ -0,0 +1,63 @@
+package com.lsy.testBoot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lance on 2023/4/6.
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
+public class Person {
+
+    private String name;
+    private int age;
+    private Map<String, Object> map;
+    private List<String> list;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, Object> map) {
+        this.map = map;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", map=" + map +
+                ", list=" + list +
+                '}';
+    }
+}
--- src/test/java/com/lsy/MySpringBootDemoApplicationTests.java
+++ src/test/java/com/lsy/MySpringBootDemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.lsy;
 
+import com.lsy.testBoot.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MySpringBootDemoApplicationTests {
 
+    @Autowired
+    private Person person;
+
     @Test
     void contextLoads() {
+        System.out.println(person.toString());
     }
 
 }
